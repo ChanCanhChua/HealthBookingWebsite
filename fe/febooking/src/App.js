04da1e6cd38e72a1b4c7a0c50ac4dd7ad5c03209
benhieuhoc <lenhatnamqt@gmail.com>
2024-12-03 22:04:54 +0700

    login logout admin  commit

---
+++ fe/febooking/src/App.js
@@ -0,0 +1,32 @@
+import{Routes , Route} from "react-router";
+import MasterLayout from "./pages/users/theme/masterLayout"
+import HomePage from "./pages/users/homePage";
+import LoginPage from "./pages/users/LoginPage";
+
+import AdminLoginPage from "./pages/admin/login";
+import AdminHomePage from "./pages/admin/homeadmin";
+import QuanLyDoctor from "./component/Admin/Doctor/QuanLyDoctor";
+
+const App = () => {
+
+    const routeConfig = [
+        { path: "/", element: <HomePage/> },
+        { path: "/login", element: <LoginPage/> },
+
+        { path: "/admin/login", element: <AdminLoginPage/> },
+        { path: "/admin/home-page", element: <AdminHomePage/> },
+        { path : "/admin/home-page/doctor-manager", element: <QuanLyDoctor/> },
+    ]
+
+    return (
+        <>
+          <Routes>
+            {routeConfig.map((route, index) => (
+                <Route key={index} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </>
+      )
+}
+
+export default App;
