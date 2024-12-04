fbcca7930311f9615e18e1c0228eca5febc4cdeb
MinhEmmmmmm <minh56140@gmail.com>
2024-12-04 17:31:09 +0700

    login_register

--- fe/febooking/src/App.js
+++ fe/febooking/src/App.js
@@ -2,11 +2,15 @@ import{Routes , Route} from "react-router";
 // import MasterLayout from "./pages/users/theme/masterLayout"
 import HomePage from "./pages/users/homePage";
 import LoginPage from "./pages/users/LoginPage";
+import RegisterPage from "./pages/users/RegisterPage";
+import DatLich from "./pages/users/DatLich";
+import ViewBacSi from "./pages/users/ViewBacSi";
 
 import AdminLoginPage from "./pages/admin/login";
 import AdminHomePage from "./pages/admin/homeadmin";
 import QuanLyDoctor from "./component/Admin/Doctor/QuanLyDoctor";
-import ViewBacSi from "./pages/users/ViewBacSi";
+
+
 
 const App = () => {
 
@@ -14,6 +18,9 @@ const App = () => {
         { path: "/", element: <HomePage/> },
         { path: "/login", element: <LoginPage/> },
         { path: "/doctor", element: <ViewBacSi/> },
+        { path: "/register", element: <RegisterPage/> },
+        { path: "/order", element: <DatLich/> },
+        
 
         { path: "/admin/login", element: <AdminLoginPage/> },
         { path: "/admin/home-page", element: <AdminHomePage/> },
